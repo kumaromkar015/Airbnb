58c984ee53c5268bca0946dcbee72c1e7d863dcd
omkar <kumaromkar015@gmail.com>
2024-12-29 14:59:56 +0530

    ignore classroom folder

--- classroom/server.js
+++ classroom/server.js
@@ -3,21 +3,47 @@ const app = express();
 const users = require("./router/users.js");
 const posts = require("./router/posts.js");
 const cookieParser = require("cookie-parser");
+const session = require("express-session");
+const flash = require("connect-flash");
+const path = require("path");
 
+//session varibles
+const sessionOptions = {
+	secret: "mysupersecretstring",
+	resave: false,
+	saveUninitialized: true,
+};
+
+//liesting port
 app.listen(3000, () => {
 	console.log("server is liestning on 3000");
 });
-app.use(cookieParser("secretcode"));
 
-app.get("/getsignedcookie", (req, res) => {
-	res.cookie("made-In", "India", { signed: true });
-	res.send("cookies sent");
+//middleware
+app.use(session(sessionOptions));
+app.use(flash());
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+app.use((req, res, next) => {
+	res.locals.successMsg = req.flash("success");
+	res.locals.errorMsg = req.flash("error");
+	next();
 });
 
-app.get("/verify", (req, res) => {
-	console.log(req.signedCookies);
-	res.send("verified");
+// Routes
+app.get("/register", (req, res) => {
+	let { name = "anomoyous" } = req.query;
+	req.session.name = name;
+
+	if (name === "anomoyous") {
+		req.flash("error", "user not registered ");
+	} else {
+		req.flash("success", "user register successfully");
+	}
+	res.redirect("/hello");
 });
 
-app.use("/users", users);
-app.use("/posts", posts);
+app.get("/hello", (req, res) => {
+	res.render("page.ejs", { name: req.session.name });
+});
